8188172bac7456af8507386b11562454c44b06e7
noracrayola <32146682+noracrayola@users.noreply.github.com>
2019-05-02 16:37:56 -0700

    Update Major.java

--- src/Major.java
+++ src/Major.java
@@ -1,26 +1,214 @@
-import java.util.ArrayList;
+package diyUniversity;
 
-public class Major {
+import java.util.List;
 
-	private String m_name;
-	private int max_courses=100;
-	// ArrayList final content should be of type Course
-	private ArrayList<String> course_list=new ArrayList <String>();
+class Major {
+	/* A class for a major in a department */
 	
-	public Major() {
-		this.m_name="void";
-		
+	private String mjrName;
+	private List<Course> courses;
+	private int numCor;
+
+
+	/*
+		Major
+			Constructor
+	
+		Inputs:
+			mjrName - name of major
+			Courses - courses that the major 
+			numCor - number of courses major has
+	
+		Outputs: None
+	
+		Return Value: The Major
+	
+		Exception: None
+	*/
+	public Major(String mjrName, List<Course> courses, int numCor) {
+		this.mjrName = mjrName;
+		this.courses = courses;
+		this.numCor = numCor;
 	}
-	// variable should be of type course at final version
-	public void addCourse(String _course) {
-		if (this.course_list.size()<=max_courses)
-			this.course_list.add(_course);
-		else
-			System.out.println("Maximum amount of courses reached");
-		
+
+
+	/*
+	setMjrName 
+	Sets the major's name
+	
+	Inputs: mjrName -  name of major
+	
+	Outputs: None
+	
+	Return Value: None
+	
+	Exception: None
+	*/
+	public void setMjrName(String mjrName) {
+		this.mjrName = mjrName;
+	}
+
+
+	/*
+	getMjrName
+		gets the major's name
+	
+	Inputs: None
+	
+	Outputs: None
+	
+	Return Value: mjrName -  name of major (String)
+	
+	Exception: None
+	*/
+	public String getMjrName() {
+		return mjrName;
+	}
+
+
+	/*
+	getNumCor
+		Gets the number of courses
+	
+	Inputs: None
+	
+	Outputs: None
+	
+	Return Value: numCor - number of courses major has (int)
+	
+	Exception: None
+	*/
+	int getNumCor(){
+		return numCor;
+	}
+
+
+
+	/*
+	setNumCor
+		Sets the number of courses
+	
+	Inputs: numCor - the number of courses a class has
+	
+	Outputs: None
+	
+	Return Value: None
+	
+	Exception: Invalid Parameter - if numCor <= 0
+	*/
+	void setNumCor(int numCor) {
+		this.numCor = numCor;
 	}
 	
-	public void deleteCourse(String _course) {
-		this.course_list.remove(_course);
+	/*
+	addCor
+		Adds course
+	
+	Inputs:
+		courseName - name of the course
+	
+	Outputs:
+		None
+	
+	Return Value:
+		None
+	
+	Exception:
+		Invalid Parameter - course already added
+	*/
+	void addCor(Course course) {
+	 	if (numCor < 100 & !courses.contains(course)) {
+	 		courses.add(course);
+	 		numCor--;
+	 		System.out.println("Course Added!");
+	 	} else {
+	 		System.out.println("Course already exists!");
+	 	}
 	}
+
+	/*
+	deleteCor
+		Deletes course
+	
+	Inputs:
+		courseName - name of the course
+		
+	Outputs:
+		None
+	
+	Return Value:
+		None
+	
+	Exception:
+		Invalid Parameter - courseName is not a valid name
+	*/
+	void deleteCor(Course course) {
+   		 	if (courses.contains(course)) {
+   		 		courses.remove(course);
+   		 		numCor--;
+   		 		System.out.println("Course Removed!");
+   		 	} else {
+   		 		System.out.println("Course doesn't exist!");
+   		 	}
+   	 }
+	
+   	/*
+	ediCourse
+	Edits a course from the major
+	
+	Inputs:  	
+	Outputs: None
+	
+	ReturnValue: None
+	
+	Exception: ParameterNotIncluded – if the major inputted does not belong the department
+   	*/
+   	public void editMjr() {
+   		//WILL DO AFTER CREATING COURSE CLASS! 
+   	}
+
+   	
+
+
+	/*
+	displayCourses
+		Display a list of courses
+	
+	Inputs:
+		None
+	
+	Outputs:
+		String list of courses
+	
+	Return Value:
+		None
+	
+	Exception:
+	None 
+	*/
+	void displayCourses() {
+		for (Course c : courses) {
+			System.out.println(c);
+		}
+	}
+	
+	public String toString() {
+		return "hell0";}
+	
+	/*
+	mjrMenu
+		Display a menu for majors
+		
+	Inputs: None
+	
+	Outputs: String displaying majors
+	
+	Return Value: None
+	
+	Exceptions: None 
+	*/
+	void mjrMenu() {}
+	
 }
+
+
