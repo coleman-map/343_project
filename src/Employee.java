b03d8b2233d48cc7b0084e8dc0f1fc7002fe06a5
Juan Garcia <45461663+GarciaJuanC@users.noreply.github.com>
2019-05-02 16:36:16 -0700

    Add files via upload

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,60 @@
 
-public class Employee {
-
+public class Employee extends Person {
+	private double salary = 0;
+	
+	/**
+	 * Default constructor employee
+	 * @param firstName- first name
+	 * @param lastName - last name
+	 * @param midIn - middle initial
+	 * @param ID - identification
+	 * @param salary - salary of employee
+	 */
+	public Employee(String firstName,String lastName,String midIntial,int ID){
+		super(firstName,lastName,midIntial,ID);
+		this.salary = 0;
+		
+	}
+	
+	
+	/**
+	 * Overloaded constructor
+	 * @param firstName- first name
+	 * @param lastName - last name
+	 * @param midIn - middle initial
+	 * @param ID - identification
+	 * * @param salary - salary of employee
+	 */
+	public Employee(String firstName,String lastName,String midIntial,int ID, double salary ){
+		super(firstName,lastName,midIntial,ID);
+		this.salary = salary;
+		
+	}
+	
+	
+	//Setters-------------------------------------------
+	/**
+	 * sets Salary
+	 * @param salary - sets salary
+	 */
+	public void setSalary(double salary){
+		this.salary = salary;
+	}
+	
+			//Getters-------------------------------------------	
+	
+	/**
+	 * get salary
+	 * @return - salary
+	 */
+	public double getSalary(){
+		return salary;	
+	}
+	
+	public String toString(){
+		return super.toString() + String.format("%-15s", salary);
+	}
+	public void createEmp(int id){
+		super.createPerson(id);
+	}
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,102 @@
 import java.util.List;
 
-public class Student {
+public class Student extends Person {
+	
+	
+	private List<String> coursesTaken;
+	private List<String> currentSchedule;
+	private Boolean paidTuition;
+	private double tuition;
+	private Major major;
+	private int currentUnits;
+	
+	
+	public Student(){
+		
+	}
+	
+	public Student(String firstName, String lastName, String midIntial, int ID) {
+		super(firstName, lastName, midIntial, ID);
+		this.setCoursesTaken(null);
+		this.setCurrentSchedule(null);
+		this.paidTuition = false;
+		this.tuition = 0;
+		this.major = null;
+		this.currentUnits = 0;
+		
+	}
+	
+	public Student(String firstName, String lastName, String midIntial, int ID, List<String> 
+					coursesTaken,List<String> currentSchedule, Boolean tuitionStatus, double tuition, 
+					Major major, int currentUnits) 
+	{
+		super(firstName, lastName, midIntial, ID);
+		this.setCoursesTaken(coursesTaken);
+		this.setCurrentSchedule(currentSchedule);
+		this.paidTuition = tuitionStatus;
+		this.tuition = tuition;
+		this.major = major;
+		this.currentUnits = currentUnits;
+		
+	}
+	
+	
+	
+	
+	public void setCoursesTaken(List<String> coursesTaken) {
+		this.coursesTaken = coursesTaken;
+	}
+	
+	
+	public void setCurrentSchedule(List<String> currentSchedule ){
+		this.currentSchedule = currentSchedule;
+	}
+
+	public void addSession(String session){
+		this.currentSchedule.add(session);
+	}
+	
+	public void dropSession(String sessionDropped){
+		while(this.currentSchedule.remove(sessionDropped));
+	}
+	
+	public void payTuition(){
+		this.paidTuition = true;
+	}
+	
+	public void viewCoursesTaken(){
+		for(int i =0; i < coursesTaken.size(); i++ ){
+			System.out.println(coursesTaken.get(i));
+		}
+	}
+	
+	public void viewCurrentSchedule(){
+		for(int i =0; i < currentSchedule.size(); i++ ){
+			System.out.println(currentSchedule.get(i));
+		}
+	}
+	
+	public void setCurrentUnits(int currentUnits){
+		this.currentUnits = currentUnits;
+	}
+	
+	public void setMajor(Major major){
+		this.major = major;
+	}
+	
+	public Major getMajor(){
+		return this.major;
+		
+	}
+	
+	public int getCurrentUnits(){
+		return this.currentUnits;
+	}
+	
+	public void createStud(int id) {
+		super.createPerson(id);
+	}
+	
+
 
 }
