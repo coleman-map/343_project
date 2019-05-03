3bfbbc6d6bb8d786340f7dfd1a9db4b74b190545
Juan Garcia <45461663+GarciaJuanC@users.noreply.github.com>
2019-05-02 17:28:41 -0700

    Add files via upload

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -9,23 +9,36 @@ public class MainMenu {
 	public static void main(String[] args){
 		TreeMap<Integer, Person> people = new TreeMap<Integer, Person>();
 		ArrayList<College> initialColleges = new ArrayList<College>();
+		//Predefined data
 		initialColleges.add(new College("Computer Science"));
+		initialColleges.add(new College("Liberal Arts"));
 		people.put(986, new Student("Juan", "Garcia", "C", 986));
+		people.put(345, new Admin("Malik", "Coleman", "K", 345, 300.00, true));
+		people.put(764, new Professor("Jon", "Tron", "T", 764, 20000.00));
+		initialColleges.get(0).setDean(people.get(764));
+		
+		//This object holds everything
 		University uni = new University("California State University, Longer Beach", initialColleges, people, 1);
+		
+		//Go time
 		menu(uni);
 		
 	}
 	
 	public static void menu(University uni){
-		Person user = login(uni);
-		
-		if(user instanceof Admin){
-			AdminMenu(uni, user);
-		}else if( user instanceof Student){
-			StudMenu(uni, user);
-		}else{
-			ProfMenu(uni, user);
-		}
+		int exit = 1;
+		do{
+			Person user = login(uni);
+			
+			if(user instanceof Admin){
+				AdminMenu(uni, user);
+			}else if( user instanceof Student){
+				StudMenu(uni, user);
+			}else{
+				ProfMenu(uni, user);
+			}
+			System.out.println("You have been logged out. \n");
+		}while(exit == 1);
 		
 		
 		
@@ -41,7 +54,9 @@ public class MainMenu {
 	}
 
 	private static void AdminMenu(University uni, Person user){
-		System.out.print("ADMININSTRATOR MENU "
+		int choice;
+		do{
+		System.out.print("\nADMININSTRATOR MENU "
 				+ "\n1. University Menu "
 				+ "\n2. College Menu "
 				+ "\n3. Department Menu"
@@ -49,7 +64,7 @@ public class MainMenu {
 				+ "\n5. Course Menu"
 				+ "\n6. Exit "
 				+ "\nEnter here: ");
-		int choice = in.nextInt();
+		choice = Integer.parseInt(in.nextLine());
 		
 		if(choice == 1){
 			uni.uniMenu();
@@ -60,17 +75,20 @@ public class MainMenu {
 		}else if(choice == 4){
 			
 		}else if(choice == 5){
+		
+		}else if(choice == 6){
 			return;
 		}
+		}while(choice != 6);
 	}
 
 	public static Person login(University uni){
 		int id = 0;
 		
 		do{
-			System.out.println("Welcome to " + uni.getUniName() + 
+			System.out.print("Welcome to " + uni.getUniName() + 
 					"!\nPlease enter your ID number: ");
-			id = in.nextInt();
+			id = Integer.parseInt(in.nextLine());
 		}while(uni.findPerson(id) == null);
 		
 		return uni.findPerson(id);
