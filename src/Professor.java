85378b585bda0fa0f626f06f366d17b46e954463
Juan Garcia <45461663+GarciaJuanC@users.noreply.github.com>
2019-05-02 17:33:14 -0700

    Add files via upload

--- src/Professor.java
+++ src/Professor.java
@@ -1,4 +1,57 @@
+import java.util.List;
 
-public class Professor {
+public class Professor extends Employee{
+
+	private List<String> schedule;
+	
+	
+	public Professor(String firstName, String lastName, String midIntial, int ID, double salary) {
+		super(firstName, lastName, midIntial, ID, salary);
+		this.setSchedule(null);
+		
+	}
+	
+	public Professor(String firstName, String lastName, String midIntial, int ID, double salary, List<String> schedule) {
+		super(firstName, lastName, midIntial, ID, salary);
+		this.setSchedule(schedule);
+		
+	}
+	
+	
+
+	/**
+	 * Gets schedule
+	 * @return the schedule
+	 */
+	public List<String> getSchedule() {
+		return schedule;
+	}
+
+	/**
+	 * Sets schedule
+	 * @param schedule the schedule to set
+	 */
+	public void setSchedule(List<String> schedule) {
+		this.schedule = schedule;
+	}
+	
+	/**
+	 * Adds session
+	 * @param session - session you are adding
+	 */
+	public void addSession(String session){
+		this.schedule.add(session);
+	}
+	
+	/**
+	 * Drops session
+	 * @param session
+	 */
+	public void dropSession(String session){
+		while(this.schedule.remove(session));  //removes all occurence of string
+	}									   //or schedule.remove(session); 
+												//removes the first String object that is equal to the
+        										// String represented by this literal
+	
 
 }
